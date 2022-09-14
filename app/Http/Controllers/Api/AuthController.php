443970e52c8a7026ccf3464f0d2f4ba1378fd879
Jan Remiel Menor <78302040+menorjanremiel@users.noreply.github.com>
2022-09-14 11:47:57 +0800

    Commit Backend

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function createUser(Request $request)
+    {
+
+        try{
+            $validateUser = Validator::make($request->all(),
+            [
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required', 'string', 'min:8']
+            ]);
+        
+            if($validateUser->fails()){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation Error',
+                    'errors' => $validateUser->errors()
+                ],401);
+            }
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User Created Succesfully',
+                'token' => $user->createToken("API TOKEN")->plainTextToken
+            ],200);
+             
+
+
+        }catch(\Throwable $th){
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ],500);
+        }
+
+        
+    }
+
+    
+    public function loginUser(Request $request)
+    {
+        try{
+            $validateUser = Validator::make($request->all(),
+            [ 
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+
+            
+            if($validateUser->fails()){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation Error',
+                    'errors' => $validateUser->errors()
+                ],401);
+            }
+
+            $login = $request->only(['email','password']);
+
+            if(!Auth::attempt($login)){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Login failed. Check your account credential.'
+                ],401);
+
+            }
+
+            $user = User::where('email', $request->email)->first();
+                return response()->json([
+                    'status' => true,
+                    'message' => 'User Logged In Succesfully',
+                    'token' => $user->createToken("API TOKEN")->plainTextToken
+                ],200);
+
+        }catch(\Throwable $th){
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ],500);
+        }
+    }
+}
